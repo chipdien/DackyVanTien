5356f79d2ba9e439009a92477f1ba1c49a194dd2
chipdien <chipdien@gmail.com>
2017-03-19 21:03:09 +0700

    Mysql Server
    Máy chủ cơ sở dữ liệu mysql

---
+++ Server/mysql.bat
@@ -0,0 +1,3 @@
+echo off
+cls
+start C:\Server\MySQL_Server_5.0\bin\mysqld-nt.exe --defaults-file=C:\Server\MySQL_Server_5.0\bin\my-small.ini
---
+++ Server/reset host.bat
@@ -0,0 +1,3 @@
+echo off
+cls
+start C:\Server\MySQL_Server_5.0\bin\mysqladmin.exe -u root -pWMeRc2xb -p 3306 flush-hosts
